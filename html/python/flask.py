57eb5b5a83425ce3fa4e8d2c5105875b957c1dc0
DP2LZabins <78356901+leonardszabins@users.noreply.github.com>
2021-11-05 17:33:52 +0000

    flask

--- html/python/flask.py
+++ html/python/flask.py
@@ -7,9 +7,17 @@ def index():
   return render_template("index.html")
 
 
-@app.route('/about')
-def about():
-  return render_template("about.html")
+@app.route('/')
+def arsti():
+  return render_template("arsti.html")
+ 
+ 
+@app.route('/')
+def kontakti():
+  return render_template("kontakti.html")
 
+@app.route('/')
+def doktorati():
+  return render_template("doktoratiunpoliklinikas.html")
 
 app.run(host='0.0.0.0', port=8080)
